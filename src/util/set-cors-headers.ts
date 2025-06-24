89e3c04728ad6fb822b841f323847a431ccd8ed0
Nitcharat Thatthiam <pondaeroz008y@gmail.com>
2025-06-24 14:31:22 +0700

    LOg origin

--- src/util/set-cors-headers.ts
+++ src/util/set-cors-headers.ts
@@ -5,7 +5,7 @@ export function setCorsHeaders(req: IncomingMessage, res: ServerResponse) {
     const origin = req.headers.origin;
     const allowedOrigins = (() => process.env.ALLOWED_ORIGINS ?? "*")();
     const allowedHeaders = (() => process.env.ALLOWED_HEADERS ?? "Content-Type, Authorization, x-api-key")();
-
+    console.log("origin", origin);
     if (origin && allowedOrigins.includes(origin)) {
         res.setHeader("Access-Control-Allow-Origin", origin);
     }
